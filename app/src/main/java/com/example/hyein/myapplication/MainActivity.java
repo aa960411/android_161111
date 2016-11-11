381c9e79602f132986265edfa96db6d5d08bf950
hyein <aa960411@naver.com>
2016-11-11 12:05:03 +0900

    버튼,체크박스,텍스트 클릭시 이벤트

--- app/src/main/java/com/example/hyein/myapplication/MainActivity.java
+++ app/src/main/java/com/example/hyein/myapplication/MainActivity.java
@@ -2,12 +2,52 @@ package com.example.hyein.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b1;
+    Button b2;
+    TextView b3;
+    CheckBox b4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1 = (Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "안녕하세요", Toast.LENGTH_SHORT).show();
+            }
+        });
+        b2 = (Button)findViewById(R.id.button2);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "다시 인사해요", Toast.LENGTH_SHORT).show();
+            }
+        });
+        b3 = (TextView)findViewById(R.id.textView);
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "새로운 세상이다", Toast.LENGTH_SHORT).show();
+            }
+        });
+        b4 = (CheckBox)findViewById(R.id.checkBox);
+        b4.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Toast.makeText(getApplicationContext(),"체크하셨습니다.",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
